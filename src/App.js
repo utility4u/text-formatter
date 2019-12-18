e1d65d49f7baaaabf8577b868b874b695bbfe1c8
utility4u <benedikthofirek@gmail.com>
2019-12-18 12:35:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import formatText from './services/formatText';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		formatText('16px Arial');
+	});
+
+	return (
+		<div id="wrapper">
+			<p className="formatedText">
+				Uživatel má možnost vkládat hodnocení konkrétních registrovaných realitních makléřů, ale Společnost
+				zakazuje urážet realitní makléře, jiné uživatele a jim blízké fyzické osoby, ať už pro jejich vyznání,
+				tělesnou vlastnost, věk nebo jinou charakteristiku, která může porušovat rovnost, a vyjadřovat se
+				rasisticky. Vulgarity jsou obecně nevhodné a je na posouzení Společnosti, zda jsou ještě
+				snesitelné. V případě, že Uživatel vloží komentář, který uvedené nerespektuje je Společnost oprávněna
+				jeho hodnocení (komentář) smazat a v případěo opakovaného poručení podmínek přístup Uživateli k webovému
+				portálu zablokovat. e a e p e o e t t t t t t t t e a e p e o e t t t t t t t t e a e p e o e t t t t t t t t</p>
+		</div>
+	);
 }
 
 export default App;
