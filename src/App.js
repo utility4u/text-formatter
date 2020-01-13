95c28b6eac9ee4ca3790c4a05b4f12f23878c898
utility4u <benedikthofirek@gmail.com>
2020-01-13 06:13:00 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import formatText from './services/formatText';
 
 function App() {
 	useEffect(() => {
-		formatText('16px Arial');
+		formatText("16px Arial");
 	});
 
 	return (
 		<div id="wrapper">
-			<p className="formatedText">
+			<p className="formattedText">
+				Uživatel má možnost vkládat hodnocení konkrétních registrovaných realitních makléřů, ale Společnost
+				zakazuje urážet realitní makléře, jiné uživatele a jim blízké fyzické osoby, ať už pro jejich vyznání,
+				tělesnou vlastnost, věk nebo jinou charakteristiku, která může porušovat rovnost, a vyjadřovat se
+				rasisticky. Vulgarity jsou obecně nevhodné a je na posouzení Společnosti, zda jsou ještě
+				snesitelné. V případě, že Uživatel vloží komentář, který uvedené nerespektuje je Společnost oprávněna
+				jeho hodnocení (komentář) smazat a v případěo opakovaného poručení podmínek přístup Uživateli k webovému
+				portálu zablokovat. e a e p e o e t t t t t t t t e a e p e o e t t t t t t t t e a e p e o e t t t t t t t t</p>
+				<p className="formattedText">
 				Uživatel má možnost vkládat hodnocení konkrétních registrovaných realitních makléřů, ale Společnost
 				zakazuje urážet realitní makléře, jiné uživatele a jim blízké fyzické osoby, ať už pro jejich vyznání,
 				tělesnou vlastnost, věk nebo jinou charakteristiku, která může porušovat rovnost, a vyjadřovat se
--- src/services/formatText.js
+++ src/services/formatText.js
@@ -1,5 +1,5 @@
 export default function formatText(font) {
-	const paragraphs = Array.from(document.getElementsByClassName('formatedText'));
+	const paragraphs = Array.from(document.querySelectorAll("[class*='formattedText']"));
 	const canvas = document.createElement('canvas').getContext('2d');
 	canvas.font = font;
 	
@@ -23,32 +23,23 @@ export default function formatText(font) {
 			
 			if(!newRow){
 				newRow += text.substring(prevWordIndex, wordIndex !== -1 ? wordIndex : text.length).trimStart();
-				console.log('FALSCH',newRow, wordIndex);
-				if(wordIndex === -1){
-				 console.log('NEWLINE', newRow, text.substring(prevWordIndex, wordIndex !== -1 ? wordIndex : text.length));
-				 }
 			}else{
 				newRow += text.substring(prevWordIndex, wordIndex !== -1 ? wordIndex : text.length);
-				console.log('RIGHT',newRow, wordIndex);
 			}
 			
-			//console.log('ZACATEK', prevWordIndex, wordIndex);
 			if (canvas.measureText(newRow).width >= maxWidth) {
 				let newRowCopy = newRow;
 				let endIndex = newRow.length;
 				let middleIndex = newRow.lastIndexOf(' ');
-				//console.log('VSTUP', newRow, newRow.length, middleIndex, endIndex);
 				
 				//if the word is not longer than the row
 				if(middleIndex !== -1){
 					//while the word is longer than possible (only first try) or the last word is too short and it is not the last word on the row 
 					//(not needed, but I want not to rely on the return value of lastIndexOf() function
 					while (canvas.measureText(newRow).width >= maxWidth || endIndex - middleIndex < 4 ) {
-						//console.log( "VNITROBLOK", canvas.measureText(newRow).width, middleIndex,endIndex);
 						endIndex = middleIndex;
 						newRow = newRow.substring(0, endIndex);
 						middleIndex = newRow.lastIndexOf(' ');
-						//console.log('AFTER', newRow, newRow.substring(0, endIndex), endIndex);
 						
 						if(middleIndex === -1){
 							break;
@@ -57,11 +48,9 @@ export default function formatText(font) {
 					
 					//if there are no long word on the row
 					if(middleIndex === -1 && endIndex < 3){
-						//console.log('POJISTKA888');
 						newRow = newRowCopy.substring(0, newRowCopy.lastIndexOf(' '));
 						wordIndex = prevWordIndex;
 						prevWordIndex = text.substring(0,wordIndex).lastIndexOf(' ');
-						//console.log('NDE', wordIndex, prevWordIndex);
 					}else{
 						//change the global string pointer, because of last word were cut and is needed on the next row
 						wordIndex -= newRowCopy.length - endIndex;
@@ -69,13 +58,11 @@ export default function formatText(font) {
 					}
 				}
 				
-				//console.log('FINAL', newRow);
 				newText += newRow + '<br/>';
 				newRow = '';
 			}else if(wordIndex === -1){
 				newText += newRow + '<br/>';
 			}
-			console.log('KONEC', prevWordIndex, wordIndex, newRow);
 		} while (wordIndex !== -1);
 
 		return newText;
